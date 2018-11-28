b66469474d84b0f3e192f47ef67be01dd312f310
sasanow32 <45412070+sasanow32@users.noreply.github.com>
2018-11-27 23:32:03 -0500

    General and Student account classes

--- Final project/Students.py
+++ Final project/Students.py
@@ -1,8 +1,23 @@
 from System import account
+
 class students(account):
-    def __init__(self):
+    def __init__(self, First, Last, DoB, Address, Email, Department, ID, Password):
+        account.__init__(self, First, Last, DoB, Address, Email)
+        self.Department = Department
+        self.ID = ID
+        self.Password = Password
 
+    def Password_Set(self, Password_old, Password_new):
+        if Password_old == self.password:
+            self.Password = Password_new
+        else:
+            print("Incorrect password \nPlease try again")
 
-    def registration(self):
-        # students need to be able to add/drop a course and determine the course load
+    def ID_Get(self):
+        return self.ID
 
+    def Password_Get(self):
+        return self.Password
+
+    def Department_Get(self):
+        return self.Department
--- Final project/System.py
+++ Final project/System.py
@@ -7,11 +7,22 @@ import Faculty
 
 class account:
 
-    def __init__(self):
-        self.f_name
-        self.l_name
-        self.DoB
-        self.address
-        self.email
+    def __init__(self, Firstname, Lastname, DoB, Address, Email):
+        self.f_name = Firstname
+        self.l_name = Lastname
+        self.DoB = DoB
+        self.address = Address
+        self.email = Email
 
+    def Get_Name(self):
+        return self.f_name + self.l_name
+
+    def Get_DoB(self):
+        return self.DoB
+
+    def Get_Address(self):
+        return self.address
+
+    def Get_Email(self):
+        return self.address
 
