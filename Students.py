c867ce929e42582f01ee8213955f5e00a9c40a42
ChronoCal <40406443+ChronoCal@users.noreply.github.com>
2018-11-27 22:40:08 -0500

    reorganising

--- Students.py
+++
@@ -1,8 +0,0 @@
-from System import account
-class students(account):
-    def __init__(self):
-
-
-    def registration(self):
-        # students need to be able to add/drop a course and determine the course load
-
