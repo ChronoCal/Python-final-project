03fc39da15187150d95548f99cad276eba24b1c5
ChronoCal <40406443+ChronoCal@users.noreply.github.com>
2018-11-27 22:36:40 -0500

    Pycharm folder

---
+++ Final project/Students.py
@@ -0,0 +1,8 @@
+from System import account
+class students(account):
+    def __init__(self):
+
+
+    def registration(self):
+        # students need to be able to add/drop a course and determine the course load
+
