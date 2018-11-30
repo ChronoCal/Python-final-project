2a550793736aab55af3162c27b3f7f8fbb0c6c33
sasanow32 <45412070+sasanow32@users.noreply.github.com>
2018-11-29 19:39:52 -0500

    create student table in account database
    set auto increment of id to start at 100000

--- Final project/DataBase.py
+++ Final project/DataBase.py
@@ -1,18 +1,47 @@
 import sqlite3 as sql
-course = sql.connect("course.db")
-c = course.cursor()
-account = sql.connect("account.db")
-a = account.cursor()
 
 
-class querry:
+class Query:
     def __init__(self):
-        if c.fetchone() == None:
-           course_create = """
-           CREATE TABLE Course(
-           course_name VARCHAR(20) NOT NULL,
-           course_desc VARCHAR(200) NOT NULL,
-           department VARCHAR(20) NOT NULL,
-           capacity int NOT NULL,
-           semester VARCHAR(6) NOT NULL,
-           PRIMARY KEY(course_name)"""
+        course = sql.connect("course.db")
+        c = course.cursor()
+        account = sql.connect("account.db")
+        a = account.cursor()
+
+        if c.fetchone() is None:
+            course_create = """
+            CREATE TABLE Course(
+            course_name VARCHAR(20) NOT NULL,
+            course_desc VARCHAR(200) NOT NULL,
+            department VARCHAR(20) NOT NULL,
+            capacity int NOT NULL,
+            semester VARCHAR(6) NOT NULL);"""
+
+            c.execute(course_create)
+
+            c.commit()
+
+            c.close()
+
+        if a.fetchone() is None:
+            account_create = """
+            Create TABLE Student(
+            student_id INT NOT NULL AUTO_INCREMENT,
+            first_name VARCHAR(20),
+            last_name VARCHAR(30),
+            address VARCHAR(30),
+            department VARCHAR(20),
+            email VARCHAR(30),
+            PRIMARY KEY (student_id) );"""
+
+            increment_id = """
+            ALTER TABLE Student AUTO_INCREMENT=100000"""
+
+            a.execute(account_create)
+
+            a.execute(increment_id)
+
+            a.commit()
+
+            a.close()
+
