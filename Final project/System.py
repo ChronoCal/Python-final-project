5eab9e6a93b3097e2cc3f273502df2ddb324ec7f
ChronoCal <40406443+ChronoCal@users.noreply.github.com>
2018-11-29 19:50:36 -0500

    Add files via upload

---
+++ Final project/System.py
@@ -0,0 +1,16 @@
+# Student IDs
+# names
+
+import Students
+import Faculty
+
+
+def main():
+
+    print('Welcome to the Student Registration System')
+    print('\nPlease enter login details')
+    id = input('ID: ')
+    password = input('password: ')
+    
+
+
