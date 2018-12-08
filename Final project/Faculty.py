d3c50beeb9ead4b2c4684cc07c4fbba2758375be
ChronoCal <40406443+ChronoCal@users.noreply.github.com>
2018-12-08 14:15:32 -0500

    Bar graph coplete

--- Final project/Faculty.py
+++ Final project/Faculty.py
@@ -1,5 +1,6 @@
 from Account import Account
 import matplotlib.pyplot as plt
+import numpy as np
 
 class Faculty(Account):
     def __init__(self, first, last, DoB, address, email, ID, password):
@@ -18,9 +19,9 @@ class Faculty(Account):
             elif find_course and line == '*':
                 break
 
-    def course_graph_pie(self, labels, size):
+    def course_graph_pie():
         labels = ['test1', 'test2', 'test3', 'test4']
-        size = [25, 25, 25, 25]
+        size = [125, 125, 25, 25]
         # explode (0, 0, 0, 0)
         fig1, display = plt.subplots()
         display.pie(size, labels=labels, autopct='%1.1f%%', startangle=90, shadow=True)
@@ -28,7 +29,25 @@ class Faculty(Account):
 
         plt.show()
 
+    def course_graph_bar():
+
+        plt.rcdefaults()
+        fig, course_chart = plt.subplots()
+
+        course = ['test1', 'test2', 'test3', 'test4']
+        size = [125, 125, 25, 25]
+
+        x_pos = np.arange(len(course))
+
+        course_chart.bar(x_pos, size, align='center', color='blue')
+        course_chart.set_xticks(x_pos)
+        course_chart.set_xticklabels(course)
+        course_chart.set_ylabel('Number of Students')
+        course_chart.set_title('Number of Students Enrolled per Course')
+
+        plt.show()
+
 def main():
-    Faculty.course_graph()
+    Faculty.course_graph_bar()
 
 main()
